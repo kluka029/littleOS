f2c943d74fb6072ed8399be9329e839e1e607794
kluka029 <joshklukas123@gmail.com>
2022-07-10 13:21:21 -0500

    delete object files

--- kmain.c
+++ kmain.c
@@ -1,9 +1,14 @@
 #include "framebuffer.h"
 #include "serial.h"
 #include "printk.h"
+//#include "gdt.h"
 
 int kmain()
 {
+    struct gdt {
+        unsigned int address;
+        unsigned short size;
+    } __attribute__((packed));
     printk("Hello World! Bush did %d\n", 911);
 
     return 0;
